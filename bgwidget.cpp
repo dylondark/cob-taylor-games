f24e953d383c16ad00b7cc562a2bbf12aa9aea05
Dylan Miller <dmanmiller2004@gmail.com>
2023-10-05 14:37:30 -0400

    Add bgWidget class

---
+++ bgwidget.cpp
@@ -0,0 +1,1 @@
+#include "bgwidget.h"
---
+++ bgwidget.h
@@ -0,0 +1,15 @@
+#ifndef BGWIDGET_H
+#define BGWIDGET_H
+
+#include <QWidget>
+#include <QtOpenGLWidgets/QOpenGLWidget>
+
+class bgWidget : public QOpenGLWidget
+{
+
+public:
+    bgWidget();
+    ~bgWidget();
+};
+
+#endif // BGWIDGET_H
