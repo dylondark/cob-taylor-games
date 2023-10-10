0b7994ee07cd90c0842255d044b7ee61dfe6306c
Dylan Miller <dmanmiller2004@gmail.com>
2023-10-10 16:22:17 -0400

    Paint background object entirely blue for testing

--- bgwidget.cpp
+++ bgwidget.cpp
@@ -13,5 +13,8 @@ bgWidget::~bgWidget()
 
 void bgWidget::paintEvent(QPaintEvent *event)
 {
-
+    QPainter painter;
+    painter.begin(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.fillRect(rect(), Qt::GlobalColor::blue);
 }
