6bb1d3dbb550a2c5be4c65ff5f85325271078a7a
blindKurama <humpydumpy015@gmail.com>
2023-11-21 13:56:27 -0500

    added QObjects inheritance for triviacontroller

--- include/triviacontroller.h
+++ include/triviacontroller.h
@@ -6,7 +6,7 @@
 // documentation for using c++ with QML can be found at
 // https://doc.qt.io/qt-6/qtqml-cppintegration-contextproperties.html
 
-class TriviaController
+class TriviaController : public QObject
 {
     Q_OBJECT
 public:
