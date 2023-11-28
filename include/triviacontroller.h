7b4dd99f7717a652e87a9c219b58b6521b27d710
Dylan Miller <dmanmiller2004@gmail.com>
2023-11-28 17:14:55 -0500

    Initial write headers for triviacontroller.h

--- include/triviacontroller.h
+++ include/triviacontroller.h
@@ -6,11 +6,25 @@
 // documentation for using c++ with QML can be found at
 // https://doc.qt.io/qt-6/qtqml-cppintegration-contextproperties.html
 
+using std::string;
+using std::vector;
 class TriviaController : public QObject
 {
     Q_OBJECT
 public:
     TriviaController();
+
+private:
+    struct question
+    {
+        string question;
+        string ans1, ans2, ans3, ans4;
+    };
+    void loadQuestions(); // loads questions from the file specified by questionPath into questionVec
+    string questionPath = "./gamefiles/triviaquestions.txt";
+    vector<question> questionVec; // vector containing the question structs, to be populated by loadQuestions
+    question getQuestion(); // returns a random question obj with NO REPEATS. may need some fail condition if this is called when there are no original questions left
+
 };
 
 #endif // TRIVIACONTROLLER_H
