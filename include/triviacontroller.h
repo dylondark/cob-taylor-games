2ad2fd7d91ae23f38f7c653ac76efe3fc68360c6
alex <adiluzio4@gmail.com>
2023-12-12 12:54:46 -0500

    prepared triviacontroller to have files read into it

--- include/triviacontroller.h
+++ include/triviacontroller.h
@@ -21,9 +21,14 @@ private:
         string ans1, ans2, ans3, ans4;
     };
     void loadQuestions(); // loads questions from the file specified by questionPath into questionVec
-    string questionPath = "./gamefiles/triviaquestions.txt";
+    const string questionPath = "./gamefiles/triviaquestions.txt";
     vector<question> questionVec; // vector containing the question structs, to be populated by loadQuestions
     question getQuestion(); // returns a random question obj with NO REPEATS. may need some fail condition if this is called when there are no original questions left
+    // the following lines do the same as the lines above except for answers instead of questions
+    void loadAnswers();
+    const string answerPath = ""; //* TO DO: add in file path *
+    vector<question> answerVec;
+    question getAnswer();
 
 };
 
--- src/triviacontroller.cpp
+++ src/triviacontroller.cpp
@@ -1,3 +1,76 @@
 #include "triviacontroller.h"
+#include <random>
+#include <algorithm>
 
 TriviaController::TriviaController() {}
+
+/* TriviaController::question TriviaController::loadQuestions()
+{
+    question questions;
+
+    // Copy file writing code from leadscores in leaderboardhandler
+
+    return questions;
+} */
+
+TriviaController::question TriviaController::getQuestion()
+{
+    // Use the C++ standard library's random device and generator to generate random indices
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+
+    // Create a vector to store the indices of the questionVec vector
+    static std::vector<int> indices(questionVec.size());
+    static int i = 0;
+
+    // Generate the random index
+    std::uniform_int_distribution<> dist(0, indices.size() - 1);
+    int index = indices[dist(gen)];
+
+    // Remove the chosen index from the indices vector
+    indices.erase(indices.begin() + i);
+
+    // If all the indices have been used, regenerate them
+    if (i >= indices.size())
+    {
+        indices.clear();
+        for (int j = 0; j < questionVec.size(); ++j)
+        {
+            indices.push_back(j);
+        }
+        i = 0;
+    }
+
+    // Return the randomly selected question object
+    return questionVec[index];
+}
+
+/* TriviaController::question TriviaController::loadAnswers()
+{
+    question answers;
+
+    // Same as above
+
+    return answers;
+} */
+
+TriviaController::question TriviaController::getAnswer()
+{
+    // Use the C++ standard library's random device and generator to generate random indices
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+
+    // Create a vector to store the indices of the questionVec vector
+    static std::vector<int> indices(answerVec.size());
+    static int i = 0;
+
+    // Generate the random index
+    std::uniform_int_distribution<> dist(0, indices.size() - 1);
+    int index = indices[dist(gen)];
+
+    // Remove the chosen index from the indices vector
+    indices.erase(indices.begin() + i);
+
+    // Return the randomly selected answer object
+    return answerVec[index];
+}
