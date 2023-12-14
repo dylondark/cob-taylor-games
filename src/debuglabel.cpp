b2a1239441b5b48c95f6e7022d078bb10a933000
Dylan Miller <dmanmiller2004@gmail.com>
2023-12-14 15:43:14 -0500

    Add current platform to debug label (untested on mac or windows)

--- src/debuglabel.cpp
+++ src/debuglabel.cpp
@@ -1,4 +1,5 @@
 #include "debuglabel.h"
+#include <QtSystemDetection>
 
 DebugLabel::DebugLabel(QWidget* parent) : QLabel(parent)
 {
@@ -7,11 +8,30 @@ DebugLabel::DebugLabel(QWidget* parent) : QLabel(parent)
 
 void DebugLabel::update()
 {
+    // declare string & build info
     QString txt("Build: ");
     txt.append(BUILD_INFO_BRANCH);
     txt.append(" #");
     txt.append(BUILD_INFO_COMMIT);
     txt.append(", ");
     txt.append(BUILD_INFO_DATE);
+
+    // determine OS
+    txt.append(" OS: ");
+#ifdef Q_OS_LINUX
+    txt.append("Linux, ");
+    // check existence of WAYLAND_DISPLAY envvar to determine if we are on wayland or x11
+    if (qgetenv("WAYLAND_DISPLAY") != "")
+        txt.append("Wayland");
+    else
+        txt.append("X11");
+#elif Q_OS_WINDOWS
+    txt.append("Windows");
+#elif Q_OS_MAC
+    txt.append("MacOS");
+#else
+    txt.append("Unknown");
+#endif
+
     setText(txt);
 }
