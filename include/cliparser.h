183c2653367227144ddd0751ed6fcef4b241577c
aes195 <147101184+aes195@users.noreply.github.com>
2024-03-20 20:15:28 -0400

    CliParser: Define -p and -w functions

--- include/cliparser.h
+++ include/cliparser.h
@@ -6,7 +6,6 @@
 
 namespace CliParser
 {
-
 // -p argument
 // get the path if specified by the user
 // will return "." if no path is specified or path is invalid
@@ -15,7 +14,6 @@ QString getPath();
 // -w argument
 // get whether the user has specified to use pop out windows for the games
 bool getWindow();
-
 }
 
 #endif // CLIPARSER_H
