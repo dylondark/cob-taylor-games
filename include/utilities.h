3daf08902c2b383f4aa3429bdc0bddbea6921b68
Dylan Miller <dmanmiller2004@gmail.com>
2024-04-23 17:10:30 -0400

    Utilities: format and document according to standards

--- include/utilities.h
+++ include/utilities.h
@@ -1,14 +1,33 @@
+/*
+    utilities.h
+
+    Class declaration for MainWindow.
+*/
+
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
 #include <QUrl>
 
-// this namespace is intended to house general utlity constants and functions that can be used by ANY other file in the project
+/*
+    Utilities namespace
 
+    This namespace is intended to house general utlity constants and functions that can be used by ANY other file in the project
+*/
 namespace Utilities
 {
+
+/*
+    Enum for selecting games.
+    Named in accordance to the "official game names" from the project standards.
+*/
 enum game {Trivia = 0, GuessTheLogo = 1, Tetroos = 2, Hopper = 3, Pong = 4, Checkers = 5};
 
+/*
+    Get the URL of a game QML file to be used in the creation of the game's window/widget.
+
+    Utilities::game game: Game to return the URL for
+*/
 inline QUrl getGameQML(game game)
 {
     switch (game)
@@ -26,7 +45,7 @@ inline QUrl getGameQML(game game)
     case Checkers:
         return QUrl("qrc:/qml/qml/CheckersGame.qml");
     default:
-        throw "Requested game QML file does not exist yet!"; // TODO: update this function when more qml files are added
+        throw "Requested game QML file does not exist yet!"; // TODO: better error handling than throwing a string
     }
 }
 }
