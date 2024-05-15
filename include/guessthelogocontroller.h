178f0405aa56ddeacea14172c68bba43ac86db1b
aes195 <147101184+aes195@users.noreply.github.com>
2024-05-15 15:59:59 -0400

    guessthelogocontroller.h: Format according to project standards FINAL.

--- include/guessthelogocontroller.h
+++ include/guessthelogocontroller.h
@@ -11,11 +11,6 @@
 #include <QtQml/qqmlregistration.h>
 #include <QVariantList>
 
-/*
-    documentation for using c++ with QML can be found at
-    https://doc.qt.io/qt-6/qtqml-cppintegration-definetypes.html
-*/
-
 using std::string;
 using std::vector;
 
@@ -23,10 +18,19 @@ struct GTLQuestion
 {
     Q_GADGET
 public:
+    // The question text.
     QString m_question;
-    int m_correct; // number of correct question 1-4
+
+    // number of correct questions 1-4
+    int m_correct;
+
+    // The text for the 4 possible answers.
     QString m_ans1, m_ans2, m_ans3, m_ans4;
+
+    // The path of the image for the current question.
     QString m_img;
+
+    // Properties to make these data members accessible in QML.
     Q_PROPERTY(QString question MEMBER m_question)
     Q_PROPERTY(int correct MEMBER m_correct);
     Q_PROPERTY(QString ans1 MEMBER m_ans1)
@@ -36,16 +40,21 @@ public:
     Q_PROPERTY(QString img MEMBER m_img)
 };
 
-/* GuessTheLogoController
+/*
+    GuessTheLogoController
 
     This class is the controller class for the Trivia game. It is instantiated as an object in the QML file for the game and used in the QML.
     Anything more than basic ui logic is located here. Specifically, this class loads the trivia question data from the filesystem, and
     it contains methods that can be used within QML to obtain that question data in random order.
+
+    Documentation for using c++ with QML can be found at:
+    https://doc.qt.io/qt-6/qtqml-cppintegration-definetypes.html
 */
 class GuessTheLogoController : public QObject
 {
     Q_OBJECT
     QML_ELEMENT
+
 public:
 
     /*
@@ -67,13 +76,34 @@ public:
         Returns a list of ints 1-4 but in random order. used for randomizing answer buttons
     */
     Q_INVOKABLE QVariantList randomizeFour();
+
 private:
+
+    /*
+        The base of the paths. Should be the location of the folder containing the gamefiles folder.
+        By default it is "." which (should) mean "wherever the game executable was launched from" but this doesn't always work (ahem, mac).
+        Will be set to whatever path is specified with the -p CLI parameter if it is used.
+    */
     string filepath = ".";
+
+    // The path to the question data file.
     const string QUESTION_PATH = "/gamefiles/GuessTheLogo/GTL.csv";
+
+    /*
+        The paths to the folders containing the images for the game.
+        The code appends the image location of the question onto this string to get the entire image path for the current question's image.
+    */
     const string IMG_PATHS[2];
-    vector<GTLQuestion> questionVec; // vector containing the question structs, to be populated by loadQuestions
+
+    // Vector containing the question structs, to be populated by loadQuestions.
+    vector<GTLQuestion> questionVec;
+
+    // Contains a randomized list of the question numbers.
     vector<int> questionNums;
+
+    // Current question assigned by randQuestion(). To be passed to QML via getQuestion().
     GTLQuestion currentQuestion;
+
 };
 
 #endif // GUESSTHELOGOCONTROLLER_H
