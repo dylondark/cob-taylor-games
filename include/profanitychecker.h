7245d820443cb06920290a78da6745afd7a4ed85
hshsj00 <hsj10320@gmail.com>
2024-05-16 10:45:55 -0400

    profanitychecker: format according to standards.

--- include/profanitychecker.h
+++ include/profanitychecker.h
@@ -1,3 +1,9 @@
+/*
+    profanitychecker.h
+
+    Class declaration for profanitychecker.
+ */
+
 #ifndef PROFANITYCHECKER_H
 #define PROFANITYCHECKER_H
 
@@ -7,17 +13,39 @@
 #include <vector>
 #include <string>
 
+/*
+    ProfanityChecker class
+
+    It’s an object that takes a player’s name and checks it against a list of bad words, and then returns whether it’s good or not.
+ */
 class ProfanityChecker : public QObject
 {
     Q_OBJECT
     QML_ELEMENT
+
 public:
+
+    /*
+        Constructor for ProfanityChecker.
+     */
     ProfanityChecker();
+
+    /*
+        Returns true if word is good, false if word is bad.
+
+        QString word: word to check.
+    */
     Q_INVOKABLE bool check(QString word);
 
 private:
+
+    // Stores the profane words to check against
     std::vector<QString> words;
+
+    // Path of the game folder
     std::string filepath = ".";
+
+    // Path to the database file
     const std::string FILENAME = "/profanitycheck/profanitywords.txt";
 };
 
--- src/profanitychecker.cpp
+++ src/profanitychecker.cpp
@@ -1,27 +1,36 @@
+/*
+    profanitychecker.cpp
+
+    Class definition for profanitychecker.
+ */
+
 #include "profanitychecker.h"
 #include <fstream>
 #include <QApplication>
 
+/*
+    Constructor for Profanitychecker.
+ */
 ProfanityChecker::ProfanityChecker()
 {
     // TODO: make this a utility function
     if (QApplication::arguments().length() > 1 && QApplication::arguments().at(1) == "-p")
-    {
         filepath = QApplication:: arguments().at(2).toStdString();
-    }
 
     // load file
     std::string line;
     std::ifstream fin(filepath + FILENAME);
     while (std::getline(fin, line))
-    {
         words.push_back(QString(line.c_str()).toUpper());
-    }
 
     fin.close();
 }
 
-// returns true if word is good, false if word is bad
+/*
+    Returns true if word is good, false if word is bad.
+
+    QString word: word to check.
+*/
 bool ProfanityChecker::check(QString word)
 {
     // remove spaces and ensure uppercase
