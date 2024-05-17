e930ab2719212d08a6c203ea80c83221e1aa04b8
Dylan Miller <dmanmiller2004@gmail.com>
2024-05-17 14:06:48 -0400

    Tetroos: update tetrooscontroller.h

--- include/tetrooscontroller.h
+++ include/tetrooscontroller.h
@@ -10,6 +10,7 @@
 #include <QObject>
 #include <QQmlEngine>
 #include <QTimer>
+#include <QImage>
 #include "tetroosdata.h"
 
 /*
@@ -25,19 +26,39 @@ class TetroosController : public QObject
     QML_ELEMENT
 public:
     explicit TetroosController();
+    Q_INVOKABLE bool isGameOver();
+    Q_INVOKABLE QImage getTextureAt(int posX, int posY);
+    Q_INVOKABLE int getScore();
+    Q_INVOKABLE PieceType getHoldPiece();
+    Q_INVOKABLE PieceType getCurrentPiece();
+    Q_INVOKABLE PieceType getNextPiece();
+    Q_INVOKABLE void left();
+    Q_INVOKABLE void right();
+    Q_INVOKABLE void down();
+    Q_INVOKABLE void rotateCW();
+    Q_INVOKABLE void rotateCCW();
+    Q_INVOKABLE void slam();
+    Q_INVOKABLE void hold();
 
 signals:
-    //updateBoardState();
+    void updateView();
 
 private:
+    bool gameOver;
     Block board[20][10];
     PieceType holdPiece;
     PieceType currentPiece;
     PieceType nextPiece;
     int score;
+    int activePieceID;
     QTimer gameTimer;
+    QImage textures[21];
 
-
+    void updateGame(GameAction trigger);
+    bool mergePieceLeft();
+    bool mergePieceRight();
+    bool mergePieceDown();
+    void calculateNextPiece();
 };
 
 #endif // TETROOSCONTROLLER_H
