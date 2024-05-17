fc6576456bcdf5da83ed0ebea688e514015bff14
Dylan Miller <dmanmiller2004@gmail.com>
2024-05-17 13:36:07 -0400

    Tetroos: remove QML properties from Block struct Plans change

--- include/tetroosdata.h
+++ include/tetroosdata.h
@@ -18,35 +18,29 @@ enum PieceType : char {I = 'I' , J = 'J', L = 'L', O = 'O', S = 'S', T = 'T', Z 
 */
 struct Block
 {
-    Q_GADGET
-    Q_ENUM(PieceType)
-public:
     /*
         Contains the type of piece this block belongs to.
-
-        Can be 'I', 'J', 'L', 'O', 'S', 'T', 'Z' for their respective pieces.
-        'E' for empty (not part of a piece).
     */
-    PieceType m_pieceType;
+    PieceType pieceType;
 
     /*
         Contains the rotation of the piece (if part of one).
 
         0 for right side up, -1 for rotated left, 1 for rotated right, 2 for upside down.
     */
-    int m_rotation;
+    int rotation;
 
     /*
         Unique identifier of the piece that this block belongs to.
 
         -1 if the block doesn't belong to a piece (empty).
     */
-    int m_pieceID;
+    int pieceID;
 
     /*
     `   Whether this block is part of the piece silhouette that shows where the piece will end up.
     */
-    bool m_silhouette;
+    bool silhouette;
 
     /*
         The location of this block in relation to the entire piece (if applicable).
@@ -54,15 +48,7 @@ public:
         Pieces are represented as a 4x4 grid of blocks with the bottom left corner being the origin.
         @TODO: document the piece grids.
     */
-    int m_posX, m_posY;
-
-    // Properties to make these data members accessible in QML.
-    Q_PROPERTY(PieceType pieceType MEMBER m_pieceType)
-    Q_PROPERTY(int rotation MEMBER m_rotation)
-    Q_PROPERTY(int pieceID MEMBER m_pieceID)
-    Q_PROPERTY(bool silhouette MEMBER m_silhouette)
-    Q_PROPERTY(int posX MEMBER m_posX)
-    Q_PROPERTY(int posY MEMBER m_posY)
+    int posX, posY;
 };
 
 #endif // TETROOSDATA_H
