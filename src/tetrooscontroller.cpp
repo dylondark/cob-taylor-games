2e5e5a5c3ebb1a23d5e619e6e65d6ad01bbe4b26
Dylan Miller <dmanmiller2004@gmail.com>
2024-05-17 16:29:03 -0400

    Tetroos: add function skeleton for updateGame

--- src/tetrooscontroller.cpp
+++ src/tetrooscontroller.cpp
@@ -17,3 +17,14 @@ TetroosController::TetroosController()
 
     // connect updateView signal to slot in QML
 }
+
+/*
+    Main game loop. Called every time a new action has happened.
+    Calculates the new game state in response to the action and sends the signal to QML to display it.
+
+    GameAction trigger: what action is triggering the game update.
+*/
+void TetroosController::updateGame(GameAction trigger)
+{
+
+}
