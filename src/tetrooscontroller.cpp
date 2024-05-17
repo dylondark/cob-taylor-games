0da9f80a525e2cdb12fb8aa3a40de357a727ba30
Dylan Miller <dmanmiller2004@gmail.com>
2024-05-17 15:56:03 -0400

    Tetroos: initial write constructor for TetroosController

--- src/tetrooscontroller.cpp
+++ src/tetrooscontroller.cpp
@@ -10,4 +10,10 @@
     Constructor for TetroosController.
 */
 TetroosController::TetroosController()
-{}
+    : textures(loadTextures())
+{
+    // connect gameTimer timeout signal to timerTick slot method
+    connect(&gameTimer, &QTimer::timeout, this, &TetroosController::timerTick);
+
+    // connect updateView signal to slot in QML
+}
