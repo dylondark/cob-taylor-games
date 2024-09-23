2aa2fd0dda7869698465cdbcf3bbca9146605d91
Dylan Miller <dmanmiller2004@gmail.com>
2024-09-23 15:48:44 -0400

    Tetroos: fix images application for T piece

--- include/tetroosdata.h
+++ include/tetroosdata.h
@@ -136,8 +136,8 @@ const PieceGrid S_PIECE = {{{{{false, 0, 3}, {false, 1, 3}, {false, 2, 3}, {fals
 // Grid representation of the T piece
 const PieceGrid T_PIECE = {{{{{false, 0, 3}, {false, 1, 3}, {false, 2, 3}, {false, 3, 3}}},
                             {{{false, 0, 2}, {false, 1, 2}, {false, 2, 2}, {false, 3, 2}}},
-                            {{{false, 0, 1}, {true , 1, 1}, {false, 2, 1}, {false, 3, 1}}},
-                            {{{true , 0, 0}, {true , 1, 0}, {true , 2, 0}, {false, 3, 0}}}}};
+                            {{{true , 0, 1}, {true , 1, 1}, {true , 2, 1}, {false, 3, 1}}},
+                            {{{false, 0, 0}, {true , 1, 0}, {false, 2, 0}, {false, 3, 0}}}}};
 
 // Grid representation of the Z piece
 const PieceGrid Z_PIECE = {{{{{false, 0, 3}, {false, 1, 3}, {false, 2, 3}, {false, 3, 3}}},
