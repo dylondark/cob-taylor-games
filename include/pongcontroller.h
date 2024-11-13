bad8732dfb27ded797ba77f8f4d61e582ffb5df6
blindKurama <humpydumpy015@gmail.com>
2024-11-13 15:36:01 -0500

    pong: wrote pongcontroller.h

--- include/pongcontroller.h
+++ include/pongcontroller.h
@@ -1,6 +1,8 @@
 #ifndef PONGCONTROLLER_H
 #define PONGCONTROLLER_H
 
+//#include "pongdata.h"
+#include "tetroosdata.h"
 #include <QObject>
 #include <QQmlEngine>
 #include <QQuickPaintedItem>
@@ -43,12 +45,59 @@ public:
     Q_INVOKABLE unsigned getScore();
 
 signals:
+    /*
+        Signal to tell QML when it's time to update the game field
+    */
+    void updateField();
 
+private slots:
 
-public slots:
-
+    /*
+        Slot function that is called when gameTimer ticks.
+    */
+    void timerTick();
 
 private:
+
+
+    // Holds the game over state for the game. No game logic will continue after this is set to true
+    bool gameOver;
+
+    // Moves the game objects (ball and paddles) based on their velocities and input
+    void move();
+
+    // Checks for collisions between the ball and walls, paddles, and adjusts direction accordingly
+    void checkCollisions();
+
+    // Manages AI behavior for the AI paddle ( e.g., tracking ball movement)
+    void aiOperation();
+
+    //Paddle playerPaddle;
+    //Paddle aiPaddle;
+    //Ball ball;
+    //AI ai;
+
+    // Timer for the game
+    QTimer gameTimer;
+
+    /*
+        Main game loop. Called every time a new action has happened.
+        Calculates the new game state in response to the action and sends the signal to QML to display it.
+
+        GameAction trigger: what action is triggering the game update.
+    */
+    void updateGame(GameAction trigger);
+
+    /*
+        checks to track player's input for paddle movement
+    */
+    // True if player is moving paddle right
+    bool movingRight;
+
+    // True if player is moving paddle left
+    bool movingLeft;
+
+
 
 };
 
