4d524b04eda97896efeefc91d7a5122c00b9733e
blindKurama <humpydumpy015@gmail.com>
2024-11-18 19:32:47 -0500

    pong: modified .h file, started working on .cpp file

--- include/pongcontroller.h
+++ include/pongcontroller.h
@@ -1,12 +1,13 @@
 #ifndef PONGCONTROLLER_H
 #define PONGCONTROLLER_H
 
-//#include "pongdata.h"
 #include "tetroosdata.h"
+#include "threadworker.h"
 #include <QObject>
 #include <QQmlEngine>
 #include <QQuickPaintedItem>
 #include <QTimer>
+#include <QThread>
 
 class PongController : public QQuickPaintedItem
 {
@@ -59,18 +60,43 @@ private slots:
 
 private:
 
+    // The main thread used to execute game logic calculations.
+    QThread logicThread;
 
-    // Holds the game over state for the game. No game logic will continue after this is set to true
+    // Worker for the main thread.
+    ThreadWorker logicThreadWorker;
+
+    /*
+        Holds the game over state for the game. No game logic will continue after this is set to true
+    */
     bool gameOver;
 
-    // Moves the game objects (ball and paddles) based on their velocities and input
+    /*
+        Moves the game objects (ball and paddles) based on their velocities and input
+    */
     void move();
 
-    // Checks for collisions between the ball and walls, paddles, and adjusts direction accordingly
+    /*
+        Checks for collisions between the ball and walls, paddles, and adjusts direction accordingly
+    */
     void checkCollisions();
 
-    // Manages AI behavior for the AI paddle ( e.g., tracking ball movement)
+    /*
+        Manages AI behavior for the AI paddle ( e.g., tracking ball movement)
+    */
     void aiOperation();
+
+    /*
+        Player's current score
+    */
+    unsigned score;
+
+    /*
+         The current interval of the timer in ms (how many ms it takes for the piece to fall 1 row naturally).
+    */
+    unsigned timerInterval;
+
+    std::vector<std::vector<int>>* board = new std::vector<std::vector<int>>();
 
     //Paddle playerPaddle;
     //Paddle aiPaddle;
--- src/pongcontroller.cpp
+++ src/pongcontroller.cpp
@@ -1,27 +1,58 @@
 #include "pongcontroller.h"
 #include <QPainter>
 
+/*
+    Constructor for PongController
+*/
 PongController::PongController()
-    : gameOver(false), movingRight(false), movingLeft(false)
+    : QQuickPaintedItem(), gameTimer(QTimer(this))
 {
+    gameOver = false;
+    movingRight = false;
+    movingLeft = false;
+    score = 0;
+    timerInterval = 1000;
 
+    logicThreadWorker.moveToThread(&logicThread);
+    logicThread.start(QThread::HighPriority);
 }
 
 PongController::~PongController()
 {
+    // Stop the logic thread
+    logicThread.quit();
+    logicThread.wait();
 
+    // Delete the dynamically allocated board
+    delete board;
 }
+
+/*
+    Paint a new frame onto the canvas
+*/
 
 void PongController::paint(QPainter* painter)
 {
+    unsigned width = this->width() / 10;
+    unsigned height = this->height() / 20;
 
+    for (unsigned row = 0; row < 20; ++row)
+        for (unsigned col =0; col < 10; ++col)
+            painter->drawRect(width * col, height * row, width, height);
 }
 
+/*
+    Kicks off the game loop. To be called by QML after entering the game.
+*/
 void PongController::startGame()
 {
-
+    // start timer
+    gameTimer.start(1000);
 }
 
+/*
+    Returns whether game is over or not.
+*/
 bool PongController::isGameOver()
 {
     return gameOver;
@@ -32,9 +63,22 @@ unsigned PongController::getScore()
     return 0;
 }
 
+/*
+    Slot function that is called when gameTimer ticks. Calls updateGame with the TimerTick action.
+*/
 void PongController::timerTick()
 {
+    if (gameOver)
+    {
+        gameTimer.stop();
+        return;
+    }
 
+    QMetaObject::invokeMethod(&logicThreadWorker, [&]() {
+        updateGame(GameAction::Down);
+    });
+
+    gameTimer.setInterval(timerInterval);
 }
 
 void PongController::move()
