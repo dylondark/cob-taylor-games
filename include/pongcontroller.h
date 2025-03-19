3cae5efac5773334d409432ee52d04ee8bf4587c
blindKurama <humpydumpy015@gmail.com>
2025-03-19 15:30:15 -0400

    Pong: Cleaned up some codes

--- include/pongcontroller.h
+++ include/pongcontroller.h
@@ -41,30 +41,23 @@ public:
     /*
         Kicks off the game loop. To be called by QML after entering the game.
     */
-
-    void updateGame();
-    /*
-        Kicks off the game loop. To be called by QML after entering the game.
-    */
     Q_INVOKABLE void startGame();
-
-
     Q_INVOKABLE void moveLeftPaddle1();
     Q_INVOKABLE void moveRightPaddle1();
     Q_INVOKABLE void moveLeftPaddle2();
     Q_INVOKABLE void moveRightPaddle2();
-    Q_INVOKABLE unsigned getPlayerScore();
-    Q_INVOKABLE unsigned getAIScore();
+    //Q_INVOKABLE unsigned getPlayerScore();
+    //Q_INVOKABLE unsigned getAIScore();
 
     /*
         Returns whether game is over or not.
     */
-    Q_INVOKABLE bool isGameOver();
+    //Q_INVOKABLE bool isGameOver();
 
     /*
         Returns the current score.
     */
-    Q_INVOKABLE unsigned getScore();
+    //Q_INVOKABLE unsigned getScore();
 
 signals:
     /*
@@ -78,7 +71,14 @@ private:
     void keyPressEvent(QKeyEvent* event) override;
     void keyReleaseEvent(QKeyEvent* event) override;
 
+    /*
+        Kicks off the game loop. To be called by QML after entering the game.
+    */
+    void updateGame();
+
+    /*
     void initBallPosition();
+    */
 
     /*
         The base of the paths. Should be the location of the folder containing the gamefiles folder.
@@ -123,8 +123,8 @@ private:
     */
     unsigned timerInterval;
 
-    int playerScore=0; // <-- Player's Score
-    int aiScore=0;     // <-- AI's Score
+    int playerScore; // <-- Player's Score
+    int aiScore;     // <-- AI's Score
 
     void timerTick();
     void resetBall();
@@ -141,8 +141,11 @@ private:
     qreal ballHeight;
     qreal ballVelocityX;
     qreal ballVelocityY;
+
     // Updates the ball's position and handles collisions
+    /*
     void updateBallPosition();
+    */
 
     // Timer for the game
     QTimer gameTimer;
