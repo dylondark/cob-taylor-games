43bcdd90c5e77111060a63608a4b47f7f8d034cc
blindKurama <humpydumpy015@gmail.com>
2025-04-02 18:36:43 -0400

    Pong: Flipped reaction time of the AI

--- include/pongdata.h
+++ include/pongdata.h
@@ -20,7 +20,8 @@ struct Paddle
 struct Ball {
     qreal x;
     qreal y;
-    qreal radius;
+    qreal width;
+    qreal height;
     qreal dx;
     qreal dy;
 };
@@ -31,23 +32,23 @@ struct AILevel {
 };
 
 const std::array<AILevel, 17> AILevels = {{
-    {0.1,  20}, // 0:  AI is losing by 8
-    {0.2,  30}, // 1:  AI is losing by 7
-    {0.3,  40}, // 2:  AI is losing by 6
-    {0.4,  50}, // 3:  AI is losing by 5
-    {0.5,  60}, // 4:  AI is losing by 4
-    {0.6,  70}, // 5:  AI is losing by 3
-    {0.7,  80}, // 6:  AI is losing by 2
-    {0.8,  90}, // 7:  AI is losing by 1
+    {1.7,  20}, // 0:  AI is losing by 8; 1.7
+    {1.6,  30}, // 1:  AI is losing by 7; 1.6
+    {1.5,  40}, // 2:  AI is losing by 6; 1.5
+    {1.4,  50}, // 3:  AI is losing by 5
+    {1.3,  60}, // 4:  AI is losing by 4
+    {1.2,  70}, // 5:  AI is losing by 3
+    {1.1,  80}, // 6:  AI is losing by 2
+    {1.0,  90}, // 7:  AI is losing by 1; 1.0
     {0.9, 100}, // 8:  Tie
-    {1.0, 110}, // 9:  AI is winning by 1
-    {1.1, 120}, // 10: AI is winning by 2
-    {1.2, 130}, // 11: AI is winning by 3
-    {1.3, 140}, // 12: AI is winning by 4
-    {1.4, 150}, // 13: AI is winning by 5
-    {1.5, 160}, // 14: AI is winning by 6
-    {1.6, 170}, // 15: AI is winning by 7
-    {1.7, 180}  // 16: AI is winning by 8
+    {0.8, 110}, // 9:  AI is winning by 1; .8
+    {0.7, 120}, // 10: AI is winning by 2
+    {0.6, 130}, // 11: AI is winning by 3
+    {0.5, 140}, // 12: AI is winning by 4
+    {0.4, 150}, // 13: AI is winning by 5
+    {0.3, 160}, // 14: AI is winning by 6; .3
+    {0.2, 170}, // 15: AI is winning by 7; .2
+    {0.1, 180}  // 16: AI is winning by 8; .1
 }};
 
 struct AI {
